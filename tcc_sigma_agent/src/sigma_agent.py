4e9ef730dd5c90e764848b198d6b7f37ec466838
Dani <daninnot@gmail.com>
2026-05-13 23:39:49 -0300

    nó 2 e 3

--- tcc_sigma_agent/src/sigma_agent.py
+++ tcc_sigma_agent/src/sigma_agent.py
@@ -2,7 +2,9 @@ import re
 import requests     #biblioteca pra conversar com a internet (pip3 install requests)
 from typing import TypedDict
 from langchain_huggingface import HuggingFaceEmbeddings
-from langchain_community.vectorstores import Chroma
+# from langchain_community.vectorstores import Chroma       #deprecated
+from langchain_chroma import Chroma
+#from langchain_ollama import ChatOllama
 
 # >>>>>>>> máquina de estados <<<<<<<<<<
 # nó 1 = 
@@ -69,7 +71,10 @@ def no2_rag(state: GraphState) -> GraphState:
 
     # Carregamos o modelo leve de embeddings e o banco criado
     embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
-    vector_store = Chroma(persist_directory="./chroma_db", embedding_function=embeddings)
+    vector_store = Chroma(
+        persist_directory="../data/chroma_db", 
+        embedding_function=embeddings
+    )
 
     # Buscamos as 2 regras que mais se aproximam do contexto pedido
     # Usamos k=2 para não sobrecarregar a memória do nosso pequeno Qwen 2.5
@@ -87,22 +92,22 @@ def no2_rag(state: GraphState) -> GraphState:
 # 5. TESTANDO O NÓ 2
 # ==========================================
 # Atualize o bloco final do arquivo para testarmos a passagem de bastão:
-if __name__ == "__main__":
+#if __name__ == "__main__":
     # Teste de integração: Nó 1 passando para o Nó 2
-    estado_inicial = {"input_usuario": "Crie uma regra para detectar a execução do mimikatz na memória."}
+#    estado_inicial = {"input_usuario": "Crie uma regra para detectar a execução do mimikatz na memória."}
     
     # Executa o Nó 1
-    estado_atualizado_1 = no_1_classificador(estado_inicial)
+#    estado_atualizado_1 = no1_classificador(estado_inicial)
     
     # O LangGraph junta os estados nos bastidores, então vamos emular isso:
-    estado_inicial.update(estado_atualizado_1) 
+#    estado_inicial.update(estado_atualizado_1) 
     
     # Executa o Nó 2
-    estado_atualizado_2 = no_2_rag(estado_inicial)
+#    estado_atualizado_2 = no2_rag(estado_inicial)
     
-    print("\n[RESULTADO FINAL DO ESTADO]")
-    print(f"Tipo: {estado_atualizado_1['tipo_input']}")
-    print(f"Tamanho do Contexto RAG gerado: {len(estado_atualizado_2['contexto_rag'])} caracteres")
+#    print("\n[RESULTADO FINAL DO ESTADO]")
+#    print(f"Tipo: {estado_atualizado_1['tipo_input']}")
+#    print(f"Tamanho do Contexto RAG gerado: {len(estado_atualizado_2['contexto_rag'])} caracteres")
 
 
 #>>>>>> NÓ 3 (API) <<<<<< 
@@ -110,13 +115,16 @@ if __name__ == "__main__":
 #Se a API falhar ou não tiver internet, o agente não dá erro, vai seguir em frente.
 def no_3_api(state: GraphState) -> GraphState:
 
+#
     print("||Nó 3|| Buscando dados em APIs externas...")
+    
+    #opção 1: CVE
     tipo = state["tipo_input"]
     termo = state["termo_busca"]
     contexto_api = "Nenhum dado externo coletado."      #inicialização da váriavel que vai pegar esse contexto
 
     if tipo == "cve"    and termo:
-        print(f"Consultando MITRE para '{termo}'...")
+        print(f"--> Consultando MITRE para '{termo}'. . .")
         url = f"https://cveawg.mitre.org/api/cve/{termo}"       #API pública e gratuita que não exige API key
 
         try:
@@ -124,4 +132,42 @@ def no_3_api(state: GraphState) -> GraphState:
             if resposta.status_code == 200:     # ?
                 dados = resposta.json()
 
-                descricoes = 
+                descricoes = dados.get("containers", {}).get("cna", {}).get("descriptions", [])     #vai navegar no JSON p achar a descrição em inglês
+                if descricoes:
+                    texto_descricao = descricoes[0].get("value", "Descrição indisponível.")
+                    contexto_api = f"Informação do MITRE para {termo}: {texto_descricao}"
+                    print(" --> Dados extraídos com sucesso da web.")
+                else:
+                    print(f" --> CVE não encontrado na API ou erro {resposta.status_code}.")
+        except Exception as e:
+            print(f" --> Falha de conexão com API: {e}")
+
+    #opção 2: Hash
+    elif tipo == "hash" and termo:
+        print(f"--> Consultando plataforma para a hash '{termo}'. . .")
+        #as hashes são consultadas no virustotal.com, mas ele exige uma API key pessoal. tenho aqui uma simulação apenas para entender a lógica
+        contexto_api=(
+            f"Simulação de API: a hash {termo} foi identificada como malware"
+            f"{termo} cria processo x em diretórios temporários"
+            f"{termo} está associada à man in the middle"
+        )
+
+    #opção 3: texto livre
+    else:
+        print("--> Busca por texto livre (sem consulta de hash e CVE).")
+
+    return {"contexto_api": contexto_api}   #atualiza o graphstate com a matéria-prima técnica
+
+# ==========================================
+# 5. TESTANDO O NÓ 3
+# ==========================================
+# Testar se o script consegue baixar dados reais da internet.
+__name__ == "__main__"
+estado_teste = {
+    "input_usuário": "Preciso de uma regra Sigma para o CVE-2021-44228",
+    "tipo_input": "cve",
+    "termo_busca": "CVE-2021-44228"     #log4shell
+}
+estado_atualizado = no_3_api(estado_teste)
+print("\n~*Resultado do contexto da API*~")
+print(estado_atualizado["contexto_api"])
